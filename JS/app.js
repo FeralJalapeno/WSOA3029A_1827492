2b9f304e854a41e3ddf612212858b54d51ae2790
FeralJalapeno <60913865+FeralJalapeno@users.noreply.github.com>
2020-07-26 00:06:44 +0200

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-const sections = document.querySelectorAll('section');
+/*const sections = document.querySelectorAll('section');
 const box = document.querySelector('.box');
 const gradients =[
     " linear-gradient(to right top, #ed4264, #ffedbc)",
@@ -13,5 +13,26 @@ const options= {
 let observer = new IntersectionObserver (navCheck, options);
 
 function navCheck(entries){
-    
+entries.forEach(entry =>{
+const className = entry.target.className;
+console.activeAnchor =document.querySelector('[data-page=${className}]');
+const gradientIndex= entry.target.getAttribute('data-index');
+const coords = activeAnchor.getBoundingClientRect();
+const directions = {
+    height: coords.height,
+    width: coords.width,
+    top: coords.top,
+    left: coords.left,
+};
+if(entry.inIntersecting){
+    box.style.setProperty('left', '${directions.left}px');
+    box.style.setProperty('top', '${directions.top}px');
+    box.style.setProperty('width', '${directions.width}px');
+    box.style.setProperty('height', '${directions.height}px');
+
 }
+});
+}
+sections.forEach(section =>{
+  observer.observe(section);  
+});*/
